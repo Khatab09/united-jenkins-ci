8ba3b3ad24fa76f313302c71d6cb98f915929e60
khatab <khtabb133@gmail.com>
2021-12-14 10:47:29 -0500

    fix

--- src/test/java/com/united/base/RunnerTest.java
+++ src/test/java/com/united/base/RunnerTest.java
@@ -6,16 +6,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src//test//resources//features"},
         glue = {"com/united"},
-        tags = "@test1" ,
+        tags = "@jenkins" ,
         publish = true
-//@test sanity
-//@test1
-
-
-
-
 )
-public class RunnerTests extends AbstractTestNGCucumberTests {
+public class RunnerTest extends AbstractTestNGCucumberTests {
 }
 
 
